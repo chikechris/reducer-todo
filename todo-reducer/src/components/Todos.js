358a5abaeec0ebdf228490ecc71797f30ce886f4
Chike Chris <cchike2017@gmail.com>
2019-08-14 05:11:18 -0400

    added toggle and clear functionalities

--- todo-reducer/src/components/Todos.js
+++ todo-reducer/src/components/Todos.js
@@ -2,14 +2,50 @@ import React, { useState, useReducer } from "react";
 
 import { initialState, reducer } from "../reducers/reducer";
 
-const Todos = () => {
+const ToDos = () => {
+  const [newTodo, setNewTodo] = useState();
+
   const [state, dispatch] = useReducer(reducer, initialState);
   console.log(state);
+
+  const handleChanges = event => {
+    setNewTodo(event.target.value);
+  };
+
   return (
     <div>
-      <h2>{state.item}</h2>
+      <h1>Add To Do</h1>
+      <div>
+        <input
+          className="todo-input"
+          type="text"
+          name="newTodo"
+          value={newTodo}
+          onChange={handleChanges}
+        />
+        <button
+          onClick={() => dispatch({ type: "ADD_TODO", payload: newTodo })}
+        >
+          Add item
+        </button>
+        <button onClick={() => dispatch({ type: "CLEAR_COMPLETED" })}>
+          Clear completed
+        </button>
+      </div>
+
+      {state.todos.map(item => (
+        <div className={`to-do${item.completed ? " completed" : ""}`}>
+          <h3>{item.item}</h3>
+          <button
+            className="complete-btn"
+            onClick={() => dispatch({ type: "TOGGLE_TODO", payload: item.id })}
+          >
+            Mark complete
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Todos;
+export default ToDos;
--- todo-reducer/src/reducers/reducer.js
+++ todo-reducer/src/reducers/reducer.js
@@ -1,11 +1,40 @@
 export const initialState = {
-  item: "Learn about reducers",
-  completed: false,
-  id: 3892987589
+  todos: [
+    {
+      item: "I need to go to movies",
+      completed: false,
+      id: 10
+    }
+  ]
 };
 
 export const reducer = (state, action) => {
   switch (action.type) {
+    case "ADD_TODO":
+      return {
+        todos: [
+          ...state.todos,
+          {
+            item: action.payload,
+            completed: false,
+            id: Date.now()
+          }
+        ]
+      };
+    case "TOGGLE_TODO":
+      return {
+        ...state,
+        todos: state.todos.map(item =>
+          item.id === action.payload
+            ? { ...item, completed: !item.completed }
+            : item
+        )
+      };
+    case "CLEAR_COMPLETED":
+      return {
+        ...state,
+        todos: state.todos.filter(item => !item.completed)
+      };
     default:
       return state;
   }
